fb4005f50c9c66e91a5f3e33e6597753042f8b8e
unknown <benjah88@gmail.com>
2017-10-31 10:14:52 -0700

    draft 1

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,32 @@
+ var number = 30;
+
+    var intervalId;
+
+    $("#stop").on("click", stop);
+
+    $("#resume").on("click", run);
+
+    function run() {
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+
+      number--;
+
+      $("#show-number").html("<h2>" + number + "</h2>");
+
+      if (number === 0) {
+
+        stop();
+
+        alert("Time Up!");
+      }
+    }
+
+    function stop() {
+
+      clearInterval(intervalId);
+    }
+
+    run();
