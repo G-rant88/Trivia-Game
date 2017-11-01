ab082e6d12b49d76e87a67203e6a2de0164d9ea2
unknown <benjah88@gmail.com>
2017-10-31 19:51:20 -0700

    tues draft

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,32 +1,154 @@
- var number = 30;
+var number = 30;
+var intervalId;
+var timer = false;
+var alist = [];
 
-    var intervalId;
 
-    $("#stop").on("click", stop);
+alist.push("Hedwig");
+alist.push("Scabbers");
+alist.push("Buckbeak");
+alist.push("Fluffy");
 
-    $("#resume").on("click", run);
+alist.push("Answer2 HERE1");
+alist.push("Answer HERE2");
+alist.push("Answer HERE3");
+alist.push("Answer HERE4");
 
-    function run() {
-      intervalId = setInterval(decrement, 1000);
-    }
+alist.push("Answer3 HERE1");
+alist.push("Answer HERE2");
+alist.push("Answer HERE3");
+alist.push("Answer HERE4");
 
-    function decrement() {
+alist.push("Answer4 HERE1");
+alist.push("Answer HERE2");
+alist.push("Answer HERE3");
+alist.push("Answer HERE4");
 
-      number--;
+alist.push("Answer5 HERE1");
+alist.push("Answer HERE2");
+alist.push("Answer HERE3");
+alist.push("Answer HERE4");
 
-      $("#show-number").html("<h2>" + number + "</h2>");
 
-      if (number === 0) {
+
+var qlist = [];
+
+qlist.push("<h3>What is the name of Harry's pet owl?</h3");
+qlist.push("<h3>Question HERE2!</h3");
+qlist.push("<h3>Question HERE3!</h3");
+qlist.push("<h3>Question HERE4!</h3");
+qlist.push("<h3>Question HERE5!</h3");
+
+
+var imgs = ["assets/images/hp1.jpg", "assets/images/hp1.jpg"];
+
+function ask (i){
+  
+
+        $(".quest").append(qlist[i]);
+        $("#answer1").val(alist[i]);
+        $("#answer2").val(alist[i + 1]);
+        $("#answer3").val(alist[i + 2]);
+        $("#answer4").val(alist[i + 3]);
+
+}
+
+
+
+
+ $("#start").on("click", run);
+
+ function run() {
+       intervalId = setInterval(decrement, 1000);
+        timer = true;
+        $(".buts").css("display", "block");
+        ask(0)
+        
+
+
+
+
+        $("#start").css("display", "none");
+        $("#show-number").html("<h2>" + number + "</h2>");
+
+
+     }
+
+
+ function decrement() {
+
+     
+       number--;
+ $("#show-number").html("<h2>" + number + "</h2>");
+       
+
+       if (number === 0) {
 
         stop();
 
-        alert("Time Up!");
-      }
-    }
+ function stop() {
+  timer = false;
+    clearInterval(intervalId);
+     }
 
-    function stop() {
+}
 
-      clearInterval(intervalId);
-    }
+}
 
-    run();
+function answer (){
+
+$(".quest").html("The Correct Answer Was: " + (alist[0]));
+$(".buts").css("display", "none");
+$("#imgholder").html("<img src='assets/images/hp1.jpg'/>");
+stop();
+$("#show-number").css("display", "none");
+
+}
+
+$(".buts").click(answer);
+
+$(".buts").on("click", function(){
+
+if ($(this).attr("id") === "answer1"){
+
+$(".right").html("You were Correct!");
+
+}
+
+else {
+
+  $(".right").html("You were Wrong!");
+
+}
+
+
+});
+
+
+
+ // var items = [];
+
+ //   $.each(data, function(i, item) {
+
+ //          items.push('<li><a href="yourlink?id=' + item.UserID + '">' + item.Username + '</a></li>');
+
+ //   });
+
+
+
+    // $("#stop").on("click", stop);
+
+    // $("#resume").on("click", run);
+
+   
+
+    
+
+         
+
+
+    //   }
+    // }
+
+    
+    // run();
